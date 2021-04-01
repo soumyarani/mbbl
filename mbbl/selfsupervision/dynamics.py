ef273a20f29e179bf836c0a09f4599ea8216a464
Utkarshmishra04 <utkarsh75477@gmail.com>
2021-04-01 15:38:45 +0530

    added dynamics module

--- mbbl/selfsupervision/dynamics.py
+++ mbbl/selfsupervision/dynamics.py
@@ -51,7 +51,7 @@ class Dynamics:
 
         ################################# Latent + Inverse Dynamics Loss #########################
 
-        self._loss_latent = self.kl(self.inverse_dynamics_action, self._action)
+        self._loss_latent = tf.reduce_mean(tf.square(self.inverse_dynamics_action - self._action))
 
         self._optimizer_latent = tf.train.AdamOptimizer(learning_rate=learning_rate)
         self._train_op_latent = self._optimizer_latent.minimize(self._loss_latent)
@@ -69,23 +69,23 @@ class Dynamics:
 
         ################################# Forward Dynamics Loss #################################
 
-        self._loss_forward = self.kl(self.forward_dynamics_lstate, self._next_state)
+        self._loss_forward = tf.reduce_mean(tf.square(self.forward_dynamics_lstate - self._next_state))
 
         self._optimizer_forward = tf.train.AdamOptimizer(learning_rate=learning_rate)
         self._train_op_forward = self._optimizer_forward.minimize(self._loss_forward)
 
         #########################################################################################
 
-        # ######################## Inverse Latent Feature Layer ###################################
+        ######################## Inverse Latent Feature Layer ###################################
 
-        # self._inv_latent_nstate = self.inv_latent_block(self.forward_dynamics_lstate)
+        self._inv_latent_nstate = self.inv_latent_block(self.forward_dynamics_lstate)
 
-        # self._loss_invlatent = self.kl(self._inv_latent_nstate, self._next_state)
+        self._loss_invlatent = tf.reduce_mean(tf.square(self._inv_latent_nstate - self._next_state))
 
-        # self._optimizer_invlatent = tf.train.AdamOptimizer(learning_rate=learning_rate)
-        # self._train_op_invlatent = self._optimizer_invlatent.minimize(self._loss_invlatent)
+        self._optimizer_invlatent = tf.train.AdamOptimizer(learning_rate=learning_rate)
+        self._train_op_invlatent = self._optimizer_invlatent.minimize(self._loss_invlatent)
 
-        # ##########################################################################################
+        ##########################################################################################
 
     def latent_block(self, state_tensor):
 
@@ -95,33 +95,34 @@ class Dynamics:
 
         return tf.layers.flatten(inputs=latent_layer_out) 
 
-    # def inv_latent_block(self, latent_state_tensor):
+    def inv_latent_block(self, latent_state_tensor):
 
-    #     inv_latent_layer_1 = tf.layers.dense(inputs=tf.expand_dims(latent_state_tensor, 0), units=64, activation=tf.nn.relu, kernel_initializer=tf.zeros_initializer())
-    #     inv_latent_layer_2 = tf.layers.dense(inputs=tf.expand_dims(inv_latent_layer_1, 0), units=64, activation=tf.nn.relu, kernel_initializer=tf.zeros_initializer())
-    #     inv_latent_layer_out = tf.layers.dense(inputs=inv_latent_layer_2, units=self.state_vector_size, kernel_initializer=tf.zeros_initializer())
+        inv_latent_layer_1 = tf.layers.dense(inputs=tf.expand_dims(latent_state_tensor, 0), units=64, activation=tf.nn.relu, kernel_initializer=tf.zeros_initializer())
+        inv_latent_layer_2 = tf.layers.dense(inputs=tf.expand_dims(inv_latent_layer_1, 0), units=64, activation=tf.nn.relu, kernel_initializer=tf.zeros_initializer())
+        inv_latent_layer_out = tf.layers.dense(inputs=inv_latent_layer_2, units=self.state_vector_size, kernel_initializer=tf.zeros_initializer())
 
-    #     return tf.layers.flatten(inputs=inv_latent_layer_out)
-
-    def kl(self, x, y):
-        X = tf.distributions.Categorical(probs=x)
-        Y = tf.distributions.Categorical(probs=y)
-        return tf.distributions.kl_divergence(X, Y)
+        return tf.layers.flatten(inputs=inv_latent_layer_out)
 
     def predict(self, state, action):
-        return self._sess.run(self.forward_dynamics_lstate, {self._state: state, self._action: action})
+        return self._sess.run(self._inv_latent_nstate, {self._state: state, self._action: action})
 
     def update(self, state, action, _next_state):
         self._sess.run(self._train_op_latent, {self._state: state, self._action: action, self._next_state: next_state})
-        # self._sess.run(self._train_op_invlatent, {self._state: state, self._action: action, self._next_state: next_state})
+        self._sess.run(self._train_op_invlatent, {self._state: state, self._action: action, self._next_state: next_state})
         self._sess.run(self._train_op_forward, {self._state: state, self._action: action, self._next_state: next_state})
 
+    def get_all_losses(self, state, action, _next_state):
 
+        loss_latent = self._sess.run(self._loss_latent, {self._state: state, self._action: action, self._next_state: next_state})
+        loss_invlatent = self._sess.run(self._loss_invlatent, {self._state: state, self._action: action, self._next_state: next_state})
+        loss_forward = self._sess.run(self._loss_forward, {self._state: state, self._action: action, self._next_state: next_state})
+
+        return [loss_latent, loss_invlatent, loss_forward]
 
 state_size = 10
 action_size = 4
 latent_size = 32
-learning_rate = 0.000001
+learning_rate = 0.001
 
 state = np.random.rand(state_size)
 action = np.random.rand(action_size).reshape((1,action_size))
@@ -142,11 +143,18 @@ with tf.Session() as sess:
     model = Dynamics(sess, state_size, action_size, latent_size, learning_rate)
     
     sess.run(tf.global_variables_initializer())
-    sess.run(tf.local_variables_initializer())
+
     #create a file that stores the summary of the operation
     writer = tf.summary.FileWriter("./output", sess.graph)
     #run the session
-    model.update(state, action, next_state)
+
+    i = 0
+
+    while i < 2000:
+        model.update(state, action, next_state)
+        print(model.get_all_losses(state, action, next_state))
+        i += 1
+
     results = model.predict(state, action)
 
-print(results)
+print(results - next_state)
